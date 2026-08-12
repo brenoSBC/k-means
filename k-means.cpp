0af4022d643e2710951630859dd1e63fb7003e67
brenoSBC <sbcbreno01@gmail.com>
2026-08-12 16:35:22 -0300

    apparently working

--- k-means.cpp
+++ k-means.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <cmath>
 
+#define DATASET_PATH "dataset.txt"
 #define K 2
 
 struct Point {
@@ -18,7 +19,7 @@ struct Point {
 
 void read_dataset(std::vector<Point>& p) {
 
-	std::ifstream file("dataset.txt");
+	std::ifstream file(DATASET_PATH);
 	if(!file.is_open()) {
 		std::cerr << "Error opening file" << std::endl;
 		exit(EXIT_FAILURE);
@@ -37,7 +38,7 @@ void read_dataset(std::vector<Point>& p) {
 }
 
 double calculate_distance(Point p, Point c) {
-	return (p.x - c.x) * (p.x - c.x) + (p.y - c.y) * (p.y - c.y);
+	return std::sqrt( (p.x - c.x) * (p.x - c.x) + (p.y - c.y) * (p.y - c.y) );
 }
 
 
@@ -48,148 +49,52 @@ int main()
 	
 	read_dataset(points);
 
-	double distance{__DBL_MAX__};
-	double curr_distance{0};
+	int iterations{0};
+	bool changed{true};
+	
+	while(changed) {
+		changed = false;
+		iterations++;
 
-	for(Point& p : points) {
-		p.min_distance = distance;
+		// para cada ponto calcula a distancia do centroide mais proximo
+		for(int j = 0; j < points.size(); j++) {
+			points[j].min_distance = __DBL_MAX__;
+			int old_cluster{points[j].cluster};
 
-		for(int i = 0; i < centroides.size(); i++) {
+			for(int k = 0; k < centroides.size(); k++) {
+				double curr_distance{calculate_distance(points[j], centroides[k])};
+				
+				if(curr_distance < points[j].min_distance) {
+					points[j].min_distance = curr_distance;
+					points[j].cluster = k; // centroid index
+				}
+			}
+			if(old_cluster != points[j].cluster) changed = true;
+		}
 
-			double curr_distance{calculate_distance(p, centroides[i])};
+		// para cada centroide calcula sua media com os pontos que pertencem ao seu cluster
+		for(int m = 0; m < centroides.size(); m++) {
+			double x_sum{0};
+			double y_sum{0};
+			int    count{0};
+
+			for(int n = 0; n < points.size(); n++) {
+				if(points[n].cluster == m) {
+					x_sum += points[n].x;
+					y_sum += points[n].y;
+					count++;
+				}
+			}
 			
-			if(curr_distance < distance) {
-				distance = curr_distance;
-				p.min_distance = curr_distance;
-				p.cluster = i; // centroid index
+			if(count > 0) {
+				double x_mean{x_sum / count};
+				double y_mean{y_sum / count};
+				centroides[m] = {x_mean, y_mean};
+
+				std::cout << "CENTROIDE: " << centroides[m].x << " " << centroides[m].y << std::endl;
 			}
 		}
 	}
-	
-	int count{0};
-	int x_sum{0};
-	int y_sum{0};
-	
-
-	for(int i = 0; i < centroides.size(); i++) {
-		
-		for(Point p : points) {
-
-			if(p.cluster == i) {
-				x_sum += p.x;
-				y_sum += p.y;
-				count++;
-			}
-		}
-		double x_mean = x_sum / count;
-		double y_mean = y_sum / count;
-		
-		centroides[i] = {x_mean, y_mean};
-	}
-
-
-
-	
-
-
-	// calcular distancia entre cada centroide e cada ponto
-	// atribuir tal ponto para tal centroide
-	// tirar a nova media do centroide
-	// fazer isto ate nao modificar a media mais
-	
-	// para tirar a nova media do centroide.
-	// pegar o X e todos os pontos e fazer media aritmetica
-	// pegar o Y de todos os pontos de fazer a media aritmetica
-
-	// for(auto p : points) {
-	// 	std::cout << p.x << " " << p.y <<  std::endl; 
-	// }
-
-
-
+	std::cout << "ITERACOES: " << iterations << std::endl;
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-comentarios sobre otimizações:
-
-é possivel usar .reserve() no vector, colocando o tamanho do arquivo/quanto
-de memoria sera usada, assim ele nao ira ter custo para expandir, pois
-nao sera necessario
-
-
-
-
-
-struct Point {
-	double x, y;
-	int cluster;
-	int min_distance;
-
-	Point() : x(0.0), y(0.0), cluster(-1), min_distance(__DBL_MAX__) {}
-
-	Point(double x, double y) : x(x), y(y), cluster(-1), min_distance(__DBL_MAX__) {} 
-
-};
-
-
-
-
-void read_dataset() {
-	std::ifstream dataset("dataset.txt");
-
-	if(!dataset.is_open()) {
-		std::cerr << "Erro arquivo" << std::endl;
-		exit(EXIT_FAILURE);
-	}
-
-	std::string line;
-	double p1{}, p2{};
-	while(getline(dataset, line, '\n')) {
-		std::stringstream ss(line);
-		ss >> p1 >> p2;
-		v.push_back({p1, p2});
-	}
-}
-
-
-
-
-
-
-
-	double distance{0};
-
-	for(Point& p : points) {
-		p.min_distance = distance;
-
-		for(int i = 0; i < centroides.size(); i++) {
-
-			double curr_distance{calculate_distance(p, c)};
-			
-			if(curr_distance < distance) {
-				p.min_distance = curr_distance;
-				p.cluster = i; // centroid index
-			}
-		
-		}
-	}	
-*/
-
